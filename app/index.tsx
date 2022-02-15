926e0defac370148b8a4c9e7ca87a95adb22320b
Milan Pavlik <pavlik.mil@gmail.com>
2022-02-15 14:13:05 +0000

    fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './src/index.css';
 import store from './src/store';
