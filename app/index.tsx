09dcbd0b81aa4b17956866686bf0ee2dba661dfe
Milan Pavlik <pavlik.mil@gmail.com>
2022-02-15 13:58:03 +0000

    Use parcel to build app

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './src/index.css';
+import store from './src/store';
+import Stories from './src/Stories';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <Stories />
+    </div>
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
